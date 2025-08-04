1aad593197f621394b028d6966e9aa4e7dbb838a
Nehmiya <nehmiyatech@gmail.com>
2025-08-04 19:31:28 +0300

    Finished Adding Weekly Forecast

--- Client/src/Components/HourlyWeather.jsx
+++ Client/src/Components/HourlyWeather.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import './CSS/HourlyWeather.css'
+import { format, parse } from 'date-fns'
 
-function HourlyWeather() {
+function HourlyWeather({data}) {
   return (
-    <div>
+    <>
+      <div className="hourly-container card">
+        {
+          data.map((hour,index)=>{
+            return (
+              <div className="hour-card" key={index}>
+                <div className="hour-time">
+                  {format(
+                    parse(hour.time, "yyyy-MM-dd HH:mm", new Date()),
+                    "h a"
+                  )}
+                </div>
+                <img
+                  src={hour.condition.icon}
+                  alt="icon"
+                  className="hour-icon"
+                />
+                <div className="hour-temp">{Math.round(hour.temp_c)}°c</div>
+                <div className="hour-rain">💧 {hour.chance_of_rain}%</div>
+              </div>
+            );
+          })
+        }
+      </div>
       
-    </div>
+    </>
   )
 }
 
